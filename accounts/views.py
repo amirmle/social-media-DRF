c16b8efa8e7640361affb56935027373fe4b1cfb
amir <amir@MOLLAEI-PC.AEM.LOCAL>
2025-08-18 14:42:00 +0330

    Continue completing documents and spectacular.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from drf_spectacular.types import OpenApiTypes
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.request import Request
@@ -12,17 +13,47 @@ from post import serializers as post_serializers
 from . import serializers
 from post.models import Post
 
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view, OpenApiExample, OpenApiResponse
+
+
 # Create your views here.
 
 
-@extend_schema(tags=['accounts - accounts'])
+# region Signup
+@extend_schema_view(
+    post = extend_schema(
+       tags=['Enter - accounts'],
+        description='Singup new user',
+        request=serializers.UserCreateSerializer,
+        responses=serializers.UserCreateSerializer,
+        examples=[
+            OpenApiExample(
+                'Simple User info to signup',
+                value={
+                    'username': 'Kevin',
+                    'password': '12345678',
+                    'confirm_password': '12345678',
+                }
+            )
+        ]
+    ),
+)
 class SignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserCreateSerializer
     permission_classes = (IsNotAuthenticated,)
 
-@extend_schema(tags=['accounts - accounts'])
+# endregion
+
+# region Logout
+@extend_schema_view(
+    post = extend_schema(
+       tags=['Enter - accounts'],
+        description='Logout user - (If user is logged in)',
+
+
+    ),
+)
 class LogoutView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
     def post(self, request):
@@ -31,9 +62,9 @@ class LogoutView(APIView):
             return Response({'detail':'Logged out successfully.'},status=status.HTTP_200_OK)
         except Token.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+# endregion
 
-
-
+# region Profile template with pure APIView
 # class ProfileView(APIView):
 #     permission_classes = (permissions.IsAuthenticated,)
 #     def get(self, request):
@@ -47,34 +78,119 @@ class LogoutView(APIView):
 #             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 #         else :
 #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@extend_schema(tags=['Profile - accounts'])
+# endregion
+# region Profile - retrieve, update(put, patch)
+@extend_schema_view(
+    get=extend_schema(
+        tags=['Profile - accounts'],
+        description='Retrieve logged-in user profile',
+        responses=serializers.ProfileSerializer
+    ),
+    put=extend_schema(
+        tags=['Profile - accounts'],
+        description='Update logged-in user profile - all details needed',
+        request=serializers.ProfileSerializer,
+        examples=[
+            OpenApiExample(
+                'Simple Post Example to update',
+                value={
+                    'username' : '<USERNAME>',
+                    'firstname' : '<FIRSTNAME>',
+                    'lastname' : '<LASTNAME>',
+                    'email' : '<EMAIL>',
+                }
+            )
+        ]
+    ),
+    patch=extend_schema(
+        tags=['Profile - accounts'],
+        description='Update logged-in user profile - just changed details needed',
+        request=serializers.ProfileSerializer,
+        examples=[
+            OpenApiExample(
+                'Simple Post Example to update',
+                value={
+                    'username' : '<USERNAME>',
+                    'firstname' : '<FIRSTNAME>',
+                    'lastname' : '<LASTNAME>',
+                    'email' : '<EMAIL>',
+                }
+            )
+        ]
+    ),
+)
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = serializers.ProfileSerializer
     permission_classes = (permissions.IsAuthenticated,)
     def get_object(self):
         return self.request.user
+# endregion
 
-@extend_schema(tags=['Profile - accounts'])
+# region profile - delete
+@extend_schema(
+    tags=['Profile - accounts'],
+    description='Delete logged-in user profile',
+)
 class ProfileDeleteView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
     def delete(self, request):
         request.user.delete()
         return Response({'detail':'User deleted.'}, status=status.HTTP_200_OK)
+# endregion
 
-
-
-@extend_schema(tags=['Profile - accounts'])
+# region change password
+@extend_schema(
+    tags=['Profile - accounts'],
+    description='Change password of user',
+    request=serializers.ChangePasswordSerializer,
+    examples=[
+        OpenApiExample(
+            'Change Password example',
+            value={
+                'old_password': '<OLD_PASSWORD>',
+                'new_password': '<NEW_PASSWORD>',
+                'confirm_password': '<NEW_PASSWORD>',
+            }
+        )
+    ],
+    responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                description='Password Changed',
+                examples=[
+                    OpenApiExample(
+                        'Password Changed',
+                        status_codes=["200"]
+                    )
+                ]
+            ),
+            400: OpenApiResponse(
+                description='Error in changing password - bad request',
+                response=serializers.ChangePasswordSerializer,
+                examples=[
+                    OpenApiExample(
+                        'Error in changing password - bad request',
+                        status_codes=["400"],
+                    )
+                ]
+            ),
+        }
+)
 class ChangePasswordView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
     def patch(self, request):
-        serializer = serializers.ResetPasswordSerializer(data=request.data, context={'request': request})
+        serializer = serializers.ChangePasswordSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response({'detail':'Password changed.'})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# endregion
 
-@extend_schema(tags=['Profile - accounts'])
+# region ProfilePostListView - All logged in user posts
+@extend_schema(
+    tags=['Profile - accounts'],
+    description='All user posts',
+               )
 class ProfilePostListView(generics.ListCreateAPIView):
     serializer_class = post_serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -82,8 +198,9 @@ class ProfilePostListView(generics.ListCreateAPIView):
         return Post.objects.filter(user=self.request.user)
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+# endregion
 
-
+# region ProfilePostDetailView - logged in users post (by url)
 @extend_schema(tags=['Profile - accounts'])
 class ProfilePostDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = post_serializers.PostDetailSerializer
@@ -93,25 +210,27 @@ class ProfilePostDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
+# endregion
 
-
+# region Others profile - (username in url)
 @extend_schema(tags=['Profile others - accounts'])
 class ProfileOtherView(APIView):
     def get (self, request, username):
         user = get_object_or_404(User, username=username)
         serializer = serializers.ProfileSerializer(user, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+# endregion
 
-
+# region ProfileOtherPostListView - all posts of other users - (username in url)
 @extend_schema(tags=['Profile others - accounts'])
 class ProfileOtherPostListView(generics.ListAPIView):
     serializer_class = post_serializers.PostSerializer
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs['username'])
         return Post.objects.filter(user = user)
+# endregion
 
-
-
+# region list of followers of one user - (username in url)
 @extend_schema(tags=['Follow - accounts'])
 class FollowersView(generics.ListAPIView):
     serializer_class = serializers.FollowerSerializer
@@ -129,7 +248,9 @@ class FollowersView(generics.ListAPIView):
                 'followers':response.data
             }
         )
+# endregion
 
+# region list of followings of one user - (username in url)
 @extend_schema(tags=['Follow - accounts'])
 class FollowingView(generics.ListAPIView):
     serializer_class = serializers.FollowingSerializer
@@ -146,7 +267,9 @@ class FollowingView(generics.ListAPIView):
                 'following':response.data
             }
         )
+# endregion
 
+# region Follow (post) and Unfollow (delete) a user - (user in url)
 @extend_schema(tags=['Follow - accounts'])
 class FollowView(APIView):
     permission_classes = (permissions.IsAuthenticated,)
@@ -169,6 +292,6 @@ class FollowView(APIView):
         if deleted :
             return Response({'detail':f'{following_instance} unfollowed!'}, status=status.HTTP_200_OK)
         return Response({'detail':f'You were not following {following_instance}!'}, status=status.HTTP_404_NOT_FOUND)
-
+# endregion
 
 
